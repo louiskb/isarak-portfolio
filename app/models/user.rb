7a6e4194de321dfae6bb42c526b094406b7264b2
Louis Bourne <chifury@proton.me>
2026-03-04 12:36:10 +0700

    feat: install friendly id.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+extend FriendlyId
+friendly_id :email, use: :slugged # Use email as slug (unique)
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
