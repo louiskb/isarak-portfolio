620aa89d34110bb93cf59dd98217e8adc16350b4
Louis Bourne <chifury@proton.me>
2026-03-04 12:36:09 +0700

    feat: install devise.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+# Uncomment the line below so that login is required on ALL pages:
+# before_action :authenticate_user!
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
