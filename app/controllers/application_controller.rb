58fa76e5dd535a3eaa188d41bf7d232ccc920b3e
Louis Bourne <chifury@proton.me>
2026-03-07 10:27:49 +0700

    feat: uncomment devise protected pages.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-# Uncomment the line below so that login is required on ALL pages:
-# before_action :authenticate_user!
+  before_action :authenticate_user!
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
