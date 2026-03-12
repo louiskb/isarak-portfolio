c0e6a6c1ba6866fb82786abc5a764aa6957042ce
Louis Bourne <chifury@proton.me>
2026-03-12 14:36:43 +0700

    feat: add additional fields to new signed up users for devise.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  def configure_permitted_parameters
+    # For additional fields in `app/views/devise/registrations/new.html.erb`
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+
+    # For additional fields in `app/views/devise/registrations/edit.html.erb`
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
